d031b5f520d9dd3dd26944326d47e045302743c2
KevinGarciaR <kevingarcia1c@gmail.com>
2018-11-15 10:02:03 -0700

    RandomUser Multiple

--- randomuserelectron/js/eventos.js
+++ randomuserelectron/js/eventos.js
@@ -1,3 +1,42 @@
+var muestraUsuarios=function(){
+	fetch('https://randomuser.me/api/?results=20')
+	.then(datos=>datos.json())
+	.then(datos=>{
+		document.getElementById("primerSeccion").innerHTML=""
+		for(let i=0;i<20;i++){
+			document.getElementById("primerSeccion").innerHTML+= `
+			<article id="artFoto">
+			<img src="${datos.results[i].picture.large}" alt="" id="foto">
+		</article>
+		<article id="artDatos">
+			<label for="txtNombre">Nombre</label>
+			<div id="txtNombre" class="redondeadonorelieve">
+			${datos.results[i].name.first}
+			</div>
+			<label for="txtApellido">Apellido</label>
+			<div id="txtApellido" class="redondeadonorelieve">
+            ${datos.results[i].name.last}
+			</div>
+			<label for="txtGenero">Género</label>
+			<div id="txtGenero" class="redondeadonorelieve">
+			${datos.results[i].name.gender}
+			</div>
+			<label for="txtCorreo">Correo</label>
+			<div id="txtCorreo" class="redondeadonorelieve">
+			${datos.results[i].name.email}
+			</div>
+		</article>
+		<hr>
+			`
+		}
+		})
+}
+
+
+
+
+
+
 var muestraUsuario=function(){
 	fetch('https://randomuser.me/api/')
 	.then(datos=>datos.json())
@@ -11,4 +50,4 @@ var muestraUsuario=function(){
 }
 
 var btnUsuario= document.getElementById('btnUsuario');
-btnUsuario.addEventListener('click',muestraUsuario)
+btnUsuario.addEventListener('click',muestraUsuarios)
